d688f00a1065ec20b238cedc81ea5dada43054f0
mschlorff33 <mschlorff33@gmail.com>
2024-05-13 09:02:20 -0400

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -4,15 +4,19 @@
  */
 
 public class Direction {
-    public static final int DOWN = 0;
-    public static final int RIGHT = 1;
-    public static final int UP = 2;
-    public static final int LEFT = 3;
+    public static final int NORTH = 0;
+    public static final int NORTHEAST= 1;
+    public static final int EAST = 2;
+    public static final int SOUTHEAST = 3;
+    public static final int SOUTH = 4;
+    public static final int SOUTHWEST = 5;
+    public static final int WEST = 6;
+    public static final int NORTHWEST = 7;
 
     private int direction;
 
     public Direction(int direction) {
-        if (direction < 0 || direction > 3) {
+        if (direction < 0 || direction > 7) {
             throw new IllegalArgumentException("direction must be in range 0 to 3");
         }
         this.direction = direction;
