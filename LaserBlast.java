3268c5fa112a3351681d1acb93392d45bac914f2
mschlorff33 <mschlorff33@gmail.com>
2024-05-13 08:09:48 -0400

    Update LaserBlast.java

--- LaserBlast.java
+++ LaserBlast.java
@@ -2,35 +2,20 @@ import javax.swing.ImageIcon;
 
 public class LaserBlast extends GameObject {
 
-    
-    private int dx;
     private int dy;
-    
 
-    public Fireball(int x, int y, int direction) {
+    // creates new LaserBlast object
+    public LaserBlast(int x, int y) 
+    {
         super(x, y);
         this.setSize(17, 18);
                 this.setIcon(new ImageIcon("./images/fireball/blue_fireball_up.png"));
-                dx = 0;
-                dy = -20;
-
-             
-        
-        }
-    
-
-    /**
-     * update the character's location and image based on the dx and dy
-     * Step 1: What i did was make sure that everytime this person reaches the edge, he goes back by Dx or Dy, negating the future dx/dy movement
-     */
-
-
-    public void update() 
-    {
-        this.setLocation(this.getX() + dx, this.getY() + dy);
-
+                dy = -20
     }
 
-    
-    
+    //updates the LaserBlasts location based on dx
+    public void update() 
+    {
+        this.setLocation(this.getX(), this.getY() + dy);
+    }
 }
